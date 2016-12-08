c90b32bf46860974582564175c60865e272820ea
shoerach <shoe.rach@gmail.com>
2016-12-08 03:42:48 -0500

    LED library refactoring (untested).

--- led.c
+++ led.c
@@ -4,8 +4,6 @@
  *
  * TODO: Add better documentation, or rewrite to make more sense.
  */
-
-
 #include "led.h"
 
 
@@ -16,22 +14,25 @@ static bool_t is_initialized;
 /* Initializes the LED module.               *
  * Must be called before all other methods.  *
  * Returns whether initialization succeeded. */
-bool_t led_init(void)
-{
+bool_t led_init(void) {
     unsigned int ra;
 
-    // TODO: Someone else please explain what's going
-    // on, or re-write to make more sense :(
+    // GPFSEL{m} controls 10 GPIO pins: [m*10, m*10+9].
+    // Every 3 bits corresponds to one pin.
 
-    ra=GET32(GPFSEL4);
-    ra&=~(7<<21);
-    ra|=1<<21;
-    PUT32(GPFSEL4,ra);
-
+    // Set (turn off) green LED (GPIO pin 35).
+    // 35 / 10 -> GPFSEL3; (35 % 10) * 3 -> 15th bit
     ra=GET32(GPFSEL3);
     ra&=~(7<<15);
     ra|=1<<15;
     PUT32(GPFSEL3,ra);
+
+    // Set (turn off) red LED (GPIO pin 47).
+    // 47 / 10 -> GPFSEL4; (47 % 10) * 3 -> 21st bit
+    ra=GET32(GPFSEL4);
+    ra&=~(7<<21);
+    ra|=1<<21;
+    PUT32(GPFSEL4,ra);
 
     is_initialized = TRUE;
     return is_initialized;
@@ -39,36 +40,38 @@ bool_t led_init(void)
 
 
 /* Returns whether this module was initialized */
-bool_t led_is_initialized(void)
-{
+bool_t led_is_initialized(void) {
     return is_initialized;
 }
 
 
-/* Turns the LED on, then back off after a brief time. *
- *                                                     *
- * TODO: Currently, this spins the CPU while waiting.  *
- * Eventually, this shouldn't prevent other things     *
- * from happening in the mean time.                    */
-void led_blink(void)
-{
+/* Turns the LED on, then back off after a brief time. */
+void set_led(led_t led_pin, switch_t s) {
+    // You clear to turn on and set to turn off.
+    // Just roll with it.
+    if (s) {
+        PUT32(GPCLR1,1<<(led_pin-32));
+    } else {
+        PUT32(GPSET1,1<<(led_pin-32));  
+    }
+}
+
+
+/* Turn LEDs on and off for a brief time.
+ * TODO: Don't spin the CPU while waiting. */
+void led_blink(void) {
     unsigned int ra;
 
     if (!is_initialized)
         return;
 
-    // TODO: Someone else please explain what's going
-    // on, or re-write to make more sense :(
-
-    // Turn the LED on and wait
-    PUT32(GPSET1,1<<(47-32));
-    PUT32(GPCLR1,1<<(35-32));
-    for(ra=0;ra<0x100000;ra++)
+    set_led(RED_LED, ON);
+    set_led(GREEN_LED, OFF);
+    for (ra = 0; ra < 0x100000; ra++)
         dummy(ra);
 
-    // Turn the LED off and wait
-    PUT32(GPCLR1,1<<(47-32));
-    PUT32(GPSET1,1<<(35-32));
-    for(ra=0;ra<0x100000;ra++)
+    set_led(RED_LED, OFF);
+    set_led(GREEN_LED, ON);
+    for (ra = 0; ra < 0x100000; ra++)
         dummy(ra);
 }
--- led.h
+++ led.h
@@ -11,25 +11,41 @@
 
 #include "globals.h"
 
-
 /* Constants */
-enum
-{
-    // GPIO function select pin addresses
+enum {
+    /* GPIO function select registers.
+     * GPFSEL{m} controls 10 GPIO pins: [m*10, m*10+9].
+     * Every 3 bits corresponds to one pin. */
     GPFSEL3 = 0x3F20000C,
     GPFSEL4 = 0x3F200010,
 
-    // GPIO pin set and clear addresses
+    /* GPIO set/clear registers.
+     * Write 1 to bit n of GPSET/CLR{m} to set/clear GPIO pin m*32+n. */
     GPSET1  = 0x3F200020,
     GPCLR1  = 0x3F20002C,
 };
 
+/* Used to set LEDs. */
+typedef enum {
+    OFF = 0,
+    ON = 1,
+} switch_t;
 
-/* LED methods */
+/* Used to specify which LED to set. */
+typedef enum {
+    RED_LED = 35,       /* Red (PWR) LED GPIO pin */
+    GREEN_LED = 47,     /* Green (ACT/OK) LED GPIO pin */
+} led_t;
+
+
+/* Initialization methods. */
 bool_t led_init(void);
 bool_t led_is_initialized(void);
 
-void led_blink(void);
+/* Switches the LED on/off. */
+void set_led(led_t led, switch_t s);
 
+/* Blinks a bunch. */
+void led_blink(void);
 
 #endif
